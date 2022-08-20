b809eaa764cb38b4eaaea37fa028f934a8940831
19521352 <80874241+dattran2803@users.noreply.github.com>
2022-08-21 02:42:34 +0700

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import Content from './Content';
+import { useRef, useState, useEffect } from 'react'
 
-
+// Lưu giữ cấc giá trị qua một tham chiếu bên ngoài
+// Function Component
 
 function App() {
-  const [show, setShow] = useState(false)
+  const [count, setCount] = useState(60)
+
+  const timerId = useRef()
+  const prevCount = useRef()
+  const h1Ref = useRef()
+
+  useEffect(() => {
+    prevCount.current = count
+  }, [count])
+
+  useEffect(() => {
+    const rect = h1Ref.current.getBoundingClientRect()
+
+    console.log(rect)
+  })
+
+  const handleStart = () => {
+    timerId.current = setInterval(() => {
+      setCount(prevCount => prevCount - 1)
+    }, 1000)
+  }
+
+  const handleStop = () => {
+    clearInterval(timerId.current)
+  }
+
+  console.log(count, prevCount.current)
 
   return (
+
     <div style={{ padding: 20 }}>
-      <button onClick={() => setShow(!show)}>Toggle</button>
-      {show && <Content />}
-    </div>
+      <h1 ref={h1Ref}>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </div >
   )
 
 }
