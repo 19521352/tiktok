06f5881e6e888ed7b6d1af60dd7665cdae469222
19521352 <80874241+dattran2803@users.noreply.github.com>
2022-08-29 11:38:34 +0700

    useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
-import { useState, useMemo, useRef } from 'react'
+import { useState, useReducer } from 'react'
+
+/** useState
+ * 1. Init state: 0
+ * 2. Actions: Up (state + 1) / Down (state - 1)
+ */
+
+/** useReducer
+ * 1. Init state: 0
+ * 2. Actions: Up (state + 1) / Down (state - 1)
+ * 3. Reducer
+ * 4. Dispatch
+ */
+
+// Init state
+const initState = 0
+
+// Actions
+const UP_ACTION = 'up'
+const DOWN_ACTION = 'down'
+
+// Reducer
+const reducer = (state, action) => {
+  console.log('reducer running...')
+  switch (action) {
+    case UP_ACTION:
+      return state + 1
+    case DOWN_ACTION:
+      return state - 1
+    default:
+      throw new Error('Invalid action')
+  }
+}
+
+
 
 function App() {
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState('')
-  const [products, setProducts] = useState([])
-
-  const nameRef = useRef()
-
-  const handleSubmit = () => {
-    setProducts([...products, {
-      name,
-      price: parseInt(price)
-    }])
-    setName('')
-    setPrice('')
-
-    nameRef.current.focus()
-  }
-
-  const total = useMemo(() => {
-    const result = products.reduce((result, prod) => {
-      console.log('Tính toán lại')
-      return result + prod.price
-    }, 0)
-    return result
-  }, [products])
+  const [count, dispatch] = useReducer(reducer, initState)
 
   return (
 
     <div style={{ padding: 20 }}>
-      <input
-        ref={nameRef}
-        value={name}
-        placeholder="Enter name..."
-        onChange={e => setName(e.target.value)}
-      />
-      <br />
-      <input
-        value={price}
-        placeholder="Enter price..."
-        onChange={e => setPrice(e.target.value)}
-      />
-      <br />
-      <button onClick={handleSubmit}>Add</button>
-      <br />
-      <p>Total: {total}</p>
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>{product.name} - {product.price}</li>
-        ))}
-      </ul>
+      <h1>{count}</h1>
+      <button
+        onClick={() => dispatch(DOWN_ACTION)}
+      >
+        Down
+      </button>
+      <button
+        onClick={() => dispatch(UP_ACTION)}
+      >
+        Up
+      </button >
     </div >
   )
 
