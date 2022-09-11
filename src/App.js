e4fa663f2baa55da3dba7f3e78d4ef03fff6ea41
19521352 <80874241+dattran2803@users.noreply.github.com>
2022-09-11 16:29:43 +0700

    CSS/SASS

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import HomePage from './pages/Home'
-import NewsPage from './pages/News'
-import ContactPage from './pages/Contact'
-
 function App() {
-
-  return (
-    <div className='app'>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/news'>News</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/news' element={<NewsPage />} />
-        <Route path='/contact' element={<ContactPage />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>ReactApp</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './components/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
